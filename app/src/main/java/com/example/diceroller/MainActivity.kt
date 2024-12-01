a52f080249cd5d2f7358e4c0319f269bdee13874
Andrey Markin <magazon4ik@yandex.ru>
2024-12-01 20:16:00 +0300

    import deleted

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -15,7 +15,6 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
@@ -46,7 +45,7 @@ fun DiceWithButtonAndImage(
         .wrapContentSize(Alignment.Center)
 ) {
     var result by remember { mutableIntStateOf(1) }
-    val imageResource = when(result){
+    val imageResource = when (result) {
         1 -> R.drawable.dice_1
         2 -> R.drawable.dice_2
         3 -> R.drawable.dice_3
